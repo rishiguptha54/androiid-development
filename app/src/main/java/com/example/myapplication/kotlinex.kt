b340badf5af1db84376e870f9bacf3bd9bdf6377
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-20 10:18:44 +0530

    kotlinex

---
+++ app/src/main/java/com/example/myapplication/kotlinex.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+fun main() {
+
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+}
