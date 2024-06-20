be748feb0ede767c346f68fcef5c1ef392ebc0ea
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-20 13:39:20 +0530

    conveting java into kotlin

--- app/src/main/java/com/example/myapplication/student.kt
+++ app/src/main/java/com/example/myapplication/student.kt
@@ -1,37 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-public class student {
-    String name;
-    int age;
-    String postaladdress;
+class student(var name: String, var age: Int, var postaladdress: String)
 
-    public student(String name, int age, String postaladdress) {
-        this.name = name;
-        this.age = age;
-        this.postaladdress = postaladdress;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPostaladdress() {
-        return postaladdress;
-    }
-
-    public void setPostaladdress(String postaladdress) {
-        this.postaladdress = postaladdress;
-    }
-}
