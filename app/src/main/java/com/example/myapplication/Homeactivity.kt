7056e8dd29f1acbded28a879f6afaa7d2cb6ba9c
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-22 14:35:14 +0530

    buttonclick-log

--- app/src/main/java/com/example/myapplication/Homeactivity.kt
+++ app/src/main/java/com/example/myapplication/Homeactivity.kt
@@ -5,17 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.util.Log
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class Homeactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        fun clickHandler(view: View) {
-            Log.i("MainActivity-clickhandler","button clicked")
-        }
+        setContentView(R.layout.activity_homeactivity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
