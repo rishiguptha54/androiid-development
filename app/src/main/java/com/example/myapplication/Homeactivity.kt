873a318d0c60a72c0d2e46654860e2159f515863
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-24 14:54:42 +0530

    create and launch another activity

--- app/src/main/java/com/example/myapplication/Homeactivity.kt
+++ app/src/main/java/com/example/myapplication/Homeactivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Homeactivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,17 +13,23 @@ import android.net.Uri
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        fun clickHandler(view: View) {
-            Log.i("MainActivity-clickhandler","button clicked")
-            var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
-            startActivity(dialIntent)
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // var myEmp = Employee("abdul")
+
+
+
+    }
+
+    fun clickHandler(view: View) {
+        Log.i("MainActivity-clickhandler","button clicked")
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+        /*  var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+           var webIntent: Intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://www.ndtv.com"))
+           startActivity(webIntent)
+
+           startActivity(webIntent)*/
+
+        var hIntent:Intent = Intent(this,HomeActivity::class.java)
+        startActivity(hIntent)
     }
 }
