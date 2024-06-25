6ea6c8c811bf6183cc5287022d684f4d4bf9a96f
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-25 09:37:47 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,15 +9,45 @@ import android.util.Log
 import android.view.View
 import android.content.Intent
 import android.net.Uri
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // var myEmp = Employee("abdul")
+        // var myEmp = Employee("krishna")
+        //inflate this xml
+        setContentView(R.layout.activity_main)
+        Log.i(TAG,"activity is getting created")
+        // inflateXml()
+    }
+    //chick came out of the egg --activity is visible to the user to interact - click
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is started")
+    }
+
+    //sleep night -- incomming call ur activity goes into the background for sometime
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG,"activity has paused")
+
+    }
+
+    //death of the chick -- activity no lonnger exists in the ram -- pushed to hdd[hibernate]
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"activity is stopped")
+
+    }
 
 
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG,"activity is destroyed")
     }
 
     fun clickHandler(view: View) {
@@ -33,5 +63,13 @@ class MainActivity : AppCompatActivity() {
         var hIntent:Intent = Intent(this,HomeActivity::class.java)
         hIntent.putExtra("mykey","android-vit-abdul")
         startActivity(hIntent)
+    }
+    fun inflateXml(){
+        var nameEditText = EditText(this)
+        nameEditText.setHint("enter ur name")
+        var pwdEditText = EditText(this)
+        pwdEditText.setHint("enter ur pass")
+        var loginButton = Button(this)
+        loginButton.setText("login")
     }
 }
