f7fa105e54fc8d06475fce93d4f5175d989ed68d
saikr <krishna.22bce20126@vitapstudent.ac.in>
2024-06-25 14:58:32 +0530

    unit testing

---
+++ app/src/test/java/com/example/myapplication/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+    Calculator calculator;
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        calculator=new Calculator();
+    }
+
+    public void testAdd() {
+
+        int expected = 30;
+        int actual = calculator.add(10,20);
+        assertEquals(expected,actual);
+    }
+    public  void testMul(){
+        int exp = 20;
+        int act = calculator.multiply(5,4);
+        assertEquals(exp,act);
+    }
+
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
